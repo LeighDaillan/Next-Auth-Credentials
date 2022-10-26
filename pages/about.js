0cd1523a21eaa463b6384daa5183d0d784bf2c49
DaillanLeigh <francodaillanleigh@gmail.com>
2022-10-26 14:03:41 +0800

    Added Extra Pages with protected route

--- pages/about.js
+++ pages/about.js
@@ -15,7 +15,7 @@ const AboutUs = function () {
         <title>About | Leigh</title>
       </Head>
 
-      <div className="grid md:grid-cols-2 mx-20 gap-5 h-auto md:h-screen mb-5">
+      <div className="grid md:grid-cols-2  mx-20 gap-5 h-auto md:h-screen mb-5">
         <div className="h-3/4 self-center mb-10 md:mb-2">
           <p className="text-2xl lg:text-4xl font-bold mt-20">
             <span className="h-3 w-3 bg-yellow-500 rounded-full inline-block mb-1 mr-3 lg:mb-2"></span>
--- pages/contact.js
+++ pages/contact.js
@@ -11,7 +11,7 @@ const ContactUs = function () {
 
       <div className="bg-slate-300 h-auto w-auto p-10 font-pop">
         <div className="ml-10 mt-5">
-          <h1 className="text-3xl font-bold mb-2 ">Connect Me</h1>
+          <h1 className="text-3xl font-bold mb-2">Connect Me</h1>
           <p className="text-xl">
             We would love you respond to your queries and help you succeed.{" "}
             <br />
